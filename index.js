d3e8b6dbc5377ac5d50ca8372019acca3b6fb5be
Your Name <you@example.com>
2018-10-12 18:14:59 +0400

    modified:   index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var app = express();
-
+app.get("/google", function(req, res){
+    res.redirect("https://www.google.com/");
+ });
+ app.get("/search", function(req, res){
+    res.redirect("https://www.google.com/search?q=search%20value");
+ });
 app.get("/", function(req, res){
-   res.send("Hello world");
+   res.send("<h1>404<h1>");
 });
 
 app.listen(3000, function(){
